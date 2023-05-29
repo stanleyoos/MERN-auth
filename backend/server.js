30aa6eb6a8a3ad6994330fd411d867f25268fe71
stanleyos <stanislaw.sarka@gmail.com>
2023-05-29 12:08:14 -0500

    Add user's routes and connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import connectDB from "./config/db.js"
 const port = process.env.PORT || 5003
 import userRoutes from "./routes/userRoutes.js"
+
+connectDB()
 
 const app = express()
 
 app.use("/api/users", userRoutes)
 
 app.get("/", (req, res) => res.send("Server is ready"))
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server started at port: ${port}`)
